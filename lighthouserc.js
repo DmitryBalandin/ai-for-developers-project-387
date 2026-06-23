a87e86646f98f9b0e11c1a4a88049e3cd4ce7fe2
DmitryBalandin <balandinqwerty1983@gmail.com>
2026-06-23 18:28:06 +0500

    fix: replace deprecated --loader with --import for tsx

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:3000/'],
-      startServerCommand: 'cd backend && PORT=3000 node --loader tsx src/server.ts',
+      startServerCommand: 'cd backend && PORT=3000 node --import tsx src/server.ts',
       numberOfRuns: 3,
       startServerReadyTimeout: 60000,
     },
